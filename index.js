deb28bc684f4fb612ffd6246bd38d58f23202418
Logan Zartman <logan.zartman@utexas.edu>
2020-02-24 12:04:51 -0600

    actually rendering now

--- index.js
+++ index.js
@@ -14,7 +14,20 @@ class Visualizer {
         this.renderer = new THREE.WebGLRenderer();
         this.scene = new THREE.Scene();
         this.camera = new THREE.OrthographicCamera();
+
+        this.camera.position.x = 0;
+        this.camera.position.y = 0;
+        this.camera.position.z = -1;
+        this.camera.lookAt(0, 0, 1);
+
+        this.renderer.setClearColor(0xaaaaaa);
         this.updateRenderer(window.innerWidth, window.innerHeight);
+
+        const sphere = new THREE.Mesh(
+            new THREE.SphereGeometry(0.25),
+            new THREE.MeshBasicMaterial({color: "red"})
+        );
+        this.scene.add(sphere);
     }
 
     updateRenderer(w, h) {
@@ -24,15 +37,19 @@ class Visualizer {
     }
 
     updateCamera() {
-        this.camera.left = 0;
-        this.camera.right = this.w;
-        this.camera.top = 0;
-        this.camera.bottom = this.h;
+        const aspectRatio = this.w / this.h;
+        this.camera.left = -1;
+        this.camera.right = 1;
+        this.camera.top = -1 / aspectRatio;
+        this.camera.bottom = 1 / aspectRatio;
         this.camera.near = 0;
-        this.camera.far = 1;
+        this.camera.far = 2;
+        this.camera.updateProjectionMatrix();
     }
 
     run() {
         this.updateCamera();
+        this.renderer.render(this.scene, this.camera);
+        requestAnimationFrame(() => this.run());
     }
 }
