d823d5156041867c8d1f63eea0232c6cf2cd6986
Logan Zartman <logan.zartman@utexas.edu>
2020-02-27 18:57:05 -0600

    fix Lines handling of container size

--- src/Lines.js
+++ src/Lines.js
@@ -11,21 +11,29 @@ class Lines {
         this.setSize(0);
     }
 
+    /**
+     * Expand container to at least n*2 vertices
+     * @param {Number} n number of lines (half the number of endpoints) 
+     */
     _expandBufferTo(n) {
         if (n <= this._containerSize)
             return;
         const itemSize = 3;
-        this.geometry.setAttribute("position", new THREE.Float32BufferAttribute(n * itemSize, itemSize));
-        this.geometry.setAttribute("color", new THREE.Float32BufferAttribute(n * itemSize, itemSize));
+        this.geometry.setAttribute("position", new THREE.Float32BufferAttribute(n * 2 * itemSize, itemSize));
+        this.geometry.setAttribute("color", new THREE.Float32BufferAttribute(n * 2 * itemSize, itemSize));
         this.position = this.geometry.attributes.position;
         this.color = this.geometry.attributes.color;
         this._containerSize = n;
     }
 
+    /**
+     * Set the number of lines
+     * @param {Number} n number of lines (half the number of endpoints)
+     */
     setSize(n) {
         this._expandBufferTo(n);
         this.size = n;
-        this.geometry.setDrawRange(0, n);
+        this.geometry.setDrawRange(0, n * 2);
     }
 
     updateAttributes() {
