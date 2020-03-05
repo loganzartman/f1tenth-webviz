748f421da8d0058869b1d537657427eeda1e0ae1
Logan Zartman <logan.zartman@utexas.edu>
2020-03-05 17:50:12 -0600

    update camera controls

--- src/CameraControls.js
+++ src/CameraControls.js
@@ -1,4 +1,5 @@
 class CameraControls {
+    enabled = true;
     maxZoom = 3;
     minZoom = 0.005;
     zoomRate = 0.001;
@@ -6,45 +7,39 @@ class CameraControls {
     dragPos = new THREE.Vector3();
 
     lastUpdateTime = Date.now();
-    interpolationTime = 0.05;
+    interpolationTime = 0.1;
     dragTarget = new THREE.Vector3();
     zoomTarget = 0;
 
-    constructor(camera, domTarget = document.body) {
+    constructor(camera, domElement = document.body) {
         this.camera = camera;
-        this.domTarget = domTarget;
-        this.domTarget.addEventListener("mousemove", event => this.mouseMove(event));
-        this.domTarget.addEventListener("mousedown", event => this.mouseDown(event));
-        this.domTarget.addEventListener("mouseup", event => this.mouseUp(event));
-        this.domTarget.addEventListener("wheel", event => this.wheel(event));
+        this.domElement = domElement;
+        this.domElement.addEventListener("mousemove", event => this.mouseMove(event));
+        this.domElement.addEventListener("mousedown", event => this.mouseDown(event));
+        this.domElement.addEventListener("mouseup", event => this.mouseUp(event));
+        this.domElement.addEventListener("wheel", event => this.wheel(event));
         this.zoomTarget = this.camera.zoom;
         this.update();
     }
 
-    /** 
-     * convert clientX, clientY coordinates to normalized device coords 
-     */
-    screenToNdc(x, y) {
-        const rect = this.domTarget.getBoundingClientRect();
-        return new THREE.Vector3(
-            (x - rect.x) / rect.width * 2 - 1,
-            (y - rect.y) / rect.height * 2 - 1,
-            1
-        );
-    }
-
     update() {
+        requestAnimationFrame(() => this.update());
         const dt = (Date.now() - this.lastUpdateTime) / 1000.0;
         this.lastUpdateTime = Date.now();
+
+        if (!this.enabled)
+            return;
 
         this.camera.position.add(this.dragTarget.clone().sub(this.camera.position)
             .multiplyScalar((1 / this.interpolationTime) * dt));
         this.camera.zoom += (this.zoomTarget - this.camera.zoom) * (1 / this.interpolationTime) * dt;
-        requestAnimationFrame(() => this.update());
     }
 
     mouseDown(event) {
-        this.dragPos = this.screenToNdc(event.clientX, event.clientY);
+        if (!this.enabled)
+            return;
+
+        this.dragPos = screenToNdc(this.domElement, event.clientX, event.clientY);
         this.down = true;
         event.preventDefault();
     }
@@ -55,20 +50,26 @@ class CameraControls {
     }
 
     mouseMove(event) {
+        if (!this.enabled)
+            return;
         if (!this.down)
             return;
-        const dragDx = this.screenToNdc(event.clientX, event.clientY)
+
+        const dragDx = screenToNdc(this.domElement, event.clientX, event.clientY)
             .unproject(this.camera)
             .sub(this.dragPos.clone().unproject(this.camera))
             .multiply(new THREE.Vector3(-1, 1, 1));
         console.log(this.dragPos);
 
         this.dragTarget.add(dragDx);
-        this.dragPos.copy(this.screenToNdc(event.clientX, event.clientY));
+        this.dragPos.copy(screenToNdc(this.domElement, event.clientX, event.clientY));
         event.preventDefault();
     }
     
     wheel(event) {
+        if (!this.enabled)
+            return;
+
         this.zoomTarget = Math.max(this.minZoom, Math.min(this.maxZoom,
             this.zoomTarget * (1 + event.deltaY * -this.zoomRate)));
         event.preventDefault();
